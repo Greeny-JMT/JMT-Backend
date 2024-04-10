44993d12caeac0010d59d5fbcdd8490ee137611a
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2024-04-10 12:53:12 +0900

    project init

---
+++ src/test/java/grenny/jmt/JmtApplicationTests.java
@@ -0,0 +1,13 @@
+package grenny.jmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
